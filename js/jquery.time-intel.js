81aea3af260afed5970d5d68afc61f7ddc26ee14
Niek den Breeje <n.j.den.breeje@gmail.com>
2016-06-14 17:20:25 +0200

    Changed jQuery plugin to prototype plugin.

--- js/jquery.time-intel.js
+++ js/jquery.time-intel.js
@@ -1,14 +1,20 @@
 /**
- * jQuery Time Intel
+ * jQuery TimeIntel
  * JQuery plugin to scan pieces of text intelligently for times and durations.
  * @author Niek den Breeje
  * @version 0.1
  */
 
-(function($) {
+(function(t) {
     'use strict';
 
-    $.fn.timeIntel = function(methodOrOptions) {
-        
+    t.fn.timeIntel = function(options) {
+        var settings = $.extend({
+            lang: 'en'
+        }, options);
+
+        return this.each(function(index, el) {
+            console.log(index, el);
+        });
     };
 })(jQuery);
