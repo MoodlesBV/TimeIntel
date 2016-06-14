8f4f0b790c69474f866b642367cd4dc25d291765
Niek den Breeje <n.j.den.breeje@gmail.com>
2016-06-14 17:21:44 +0200

    Added unstaged files to commit.

---
+++ js/locale/en.js
@@ -0,0 +1,6 @@
+window.lang = {
+    "to"           : ["to", "until", "til"],
+    "hour"         : ["hour", "hours"],
+    "half-an-hour" : ["half an hour", "half hour", "half hours"],
+    "quarter"      : ["quarter", "quarters"]
+};
---
+++ js/locale/nl.js
@@ -0,0 +1,6 @@
+window.lang = {
+    "to"           : ["tot", "tot en met", "t/m", "t\\m"],
+    "hour"         : ["uur", "uren"],
+    "half-an-hour" : ["half uur", "halfuur", "halve uren"],
+    "quarter"      : ["kwartier", "kwartiertje", "kwartieren"]
+};
